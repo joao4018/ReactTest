313f4f0e3201feb75db5ef0c96cc87ca2aa2226e
joao.reis <joao.reis@valecard.com.br>
2021-06-08 16:29:36 -0300

    Update 2021-06-08T19:29:34.925Z

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var ghPages = require('gulp-gh-pages');
+
+ gulp.task('deploy', function() {
+   return gulp.src('./*')
+     .pipe(ghPages({
+        remoteUrl: 'https://github.com/joao4018/ReactTest.git',
+        branch: 'gh-pages'
+      }));
+ });
+
+// compile scss to css
+gulp.task('sass', function () {
+    return gulp.src('./sass/styles.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({basename: 'styles.min'}))
+        .pipe(gulp.dest('./css'));
+});
+
+// watch changes in scss files and run sass task
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+// minify js
+gulp.task('minify-js', function () {
+    return gulp.src('./js/scripts.js')
+        .pipe(uglify())
+        .pipe(rename({basename: 'scripts.min'}))
+        .pipe(gulp.dest('./js'));
+});
+
+// default task
+gulp.task('default', gulp.series('sass', 'minify-js'));
